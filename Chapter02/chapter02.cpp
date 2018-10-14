4c5cc88d313a9864884c4be6b29b8f7ff4108a2e
Jordi Inglada <jordi.inglada@cesbio.cnes.fr>
2018-10-14 21:59:37 +0200

    Implement get_data function

--- Chapter02/chapter02.cpp
+++ Chapter02/chapter02.cpp
@@ -1,46 +1,26 @@
 #include <torch/torch.h>
 #include <iostream>
+#include <utility>
 
-// build a neural network similar to how you would do it with Pytorch 
+using LabeledDataSet = std::pair<at::Tensor, at::Tensor>;
 
-struct Model : torch::nn::Module {
-
-    // Constructor
-    Model() {
-        // construct and register your layers
-        in = register_module("in",torch::nn::Linear(8,64));
-        h = register_module("h",torch::nn::Linear(64,64));
-        out = register_module("out",torch::nn::Linear(64,1));
-    }
-
-    // the forward operation (how data will flow from layer to layer)
-    torch::Tensor forward(torch::Tensor X){
-        // let's pass relu 
-        X = torch::relu(in->forward(X));
-        X = torch::relu(h->forward(X));
-        X = torch::sigmoid(out->forward(X));
-        
-        // return the output
-        return X;
-    }
-
-    torch::nn::Linear in{nullptr},h{nullptr},out{nullptr};
+LabeledDataSet get_data()
+{
+  auto X = torch::tensor({3.3,4.4,5.5,6.71,6.93,4.168,9.779,6.182,
+                          7.59,2.167, 7.042,10.791,5.313,7.997,5.654,9.27,
+                          3.1}, torch::kFloat32);
+  auto y = torch::tensor({1.7,2.76,2.09,3.19,1.694,1.573,3.366,2.596,
+                          2.53,1.221,2.827,3.465,1.65,2.904,2.42,2.94,
+                          1.3}, torch::kFloat32);
+  return std::make_pair(X, y);
+}
 
 
 
-};
 
 
 int main(){
-
-    Model model;
-    
-    auto in = torch::rand({8,});
-
-    auto out = model.forward(in);
-
-    std::cout << in << std::endl;
-    std::cout << out << std::endl;
-
+  std::cout << "Chapter 02\n";
+  auto train_data_set = get_data();
 }
 
